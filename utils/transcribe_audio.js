2b5979980a98401d8b99b9f0ac36078ef0780ed9
hasnzahmd <hassanahmad.6h1@gmail.com>
2024-08-21 14:08:15 +0500

    add detect lang

--- utils/transcribe_audio.js
+++ utils/transcribe_audio.js
@@ -2,16 +2,17 @@ import { createClient } from "@deepgram/sdk";
 const deepgram = createClient(process.env.DEEPGRAM_API_KEY);
 import fs from 'fs';
 
-export const transcribeAudio = async (filePath, model = 'nova', language = 'en') => {
+export const transcribeAudio = async (filePath) => {
     try {
         console.log('\n>>>>>> Start transcribe audio <<<<<<');
         const audioStream = fs.createReadStream(filePath);
+        const model = 'nova-2'
 
         const { result, error } = await deepgram.listen.prerecorded.transcribeFile(
             audioStream,
             {
                 model: model,
-                language: language,
+                detect_language: true,
                 numerals: true,
                 dictation: true,
                 smart_format: true
@@ -21,6 +22,10 @@ export const transcribeAudio = async (filePath, model = 'nova', language = 'en')
             throw error;
         }
         const transcript = result.results.channels[0].alternatives[0].transcript;
+        const language = result.results.channels[0].detected_language;
+        const languageConfidence = result.results.channels[0].language_confidence;
+        console.log('Audio language:',language);
+        console.log('Language confidence:',languageConfidence)
         console.log('Transcript:',transcript); 
 
         console.log('>>>>>> Complete transcribe audio <<<<<<'); 
